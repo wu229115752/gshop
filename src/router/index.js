3e066e997b15ca3d10798484361019fd956e977f
zhangerheba <229115752@qq.com>
2021-01-15 16:03:26 +0800

    init gshop

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import MSite from '../pages/MSite/MSite'
+import Search from '../pages/Search/Search'
+import Profile from '../pages/Profile/Profile'
+import Order from '../pages/Order/Order'
+import Login from '../pages/Login/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/msite',
+      component: MSite,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login,
+      meta: {
+        showFooter: false
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    }
+  ]
+})
